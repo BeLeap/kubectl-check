67b29cb7c05302eb2577ffeabfe479ff97ab8289
BeLeap <beleap@beleap.dev>
2024-09-30 21:22:26 +0900

    feat: get subcommand args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::{command, Arg, ArgAction};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!()
+        .arg(Arg::new("command").required(true).action(ArgAction::Append))
+        .get_matches();
+
+    let command = matches
+        .get_many::<String>("command")
+        .unwrap_or_default()
+        .collect::<Vec<_>>();
+
+    println!("{:#?}", command)
 }
