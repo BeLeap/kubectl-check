42116026273e440055738dc906d0c585574b3394
BeLeap <beleap@beleap.dev>
2024-09-30 21:30:18 +0900

    add spec

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,27 @@ fn main() {
     println!("{:#?}", command)
 }
 
+fn get_context(command: Vec<&String>) -> String {
+    todo!()
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn dummy() {}
+
+    mod get_context {
+        use crate::get_context;
+
+        #[test]
+        fn it_should_get_context_from_command() {
+            let result = get_context(vec![
+                &"kubectl".to_string(),
+                &"--context".to_string(),
+                &"test".to_string(),
+            ]);
+
+            assert_eq!(result, "test");
+        }
+    }
 }
