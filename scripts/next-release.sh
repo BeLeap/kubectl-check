64b05b3a932bde101bef38e7b25cc7e59358e666
BeLeap <beleap@beleap.dev>
2024-10-01 22:50:36 +0900

    add script

---
+++ scripts/next-release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+current_version=$(grep '^version =' Cargo.toml | sed -E 's/version = "(.*)"/\1/')
+
+head=$(echo "$current_version" | cut -d '.' -f 1)
+current_yearweek=$(echo "$current_version" | cut -d '.' -f 2)
+
+yearweek=$(date +"%y%U")
+
+if [[ "$current_yearweek" == "$yearweek" ]]; then
+  current_build=$(echo "$current_version" | cut -d '.' -f 3)
+  build=$(($current_build + 1))
+else
+  build=0
+fi
+
+new_version="$head.$yearweek.$build"
+
+sed -i.bak "s/version = \"$current_version\"/version = \"$new_version\"/" Cargo.toml
+
+git add Cargo.toml $BUILD_FILE
+git commit -m "Release version $new_version"
+
+git tag "v$new_version"
+
+git push origin main
+git push origin "v$new_version"
