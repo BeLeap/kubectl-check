b5c117f5319ca2c4681d4649c26d4186fdc0a0a5
BeLeap <beleap@beleap.dev>
2024-10-01 18:49:28 +0900

    remove clap

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,45 @@
-use clap::{command, Arg, ArgAction};
+use std::{env, fs};
+
+use yaml_rust2::YamlLoader;
 
 fn main() {
-    let matches = command!()
-        .arg(Arg::new("command").required(true).action(ArgAction::Append))
-        .get_matches();
+    let command = std::env::args().collect();
 
-    let command = matches
-        .get_many::<String>("command")
-        .unwrap_or_default()
-        .collect::<Vec<_>>();
+    let kube_config = read_kube_config();
+    let metadata = extract_metadata(kube_config, command);
 
-    println!("{:#?}", command)
+    println!("{:#?}", metadata)
 }
 
+#[derive(Clone, Debug)]
 struct KubeContextMetadata {
     namespace: Option<String>,
 }
 
+#[derive(Clone, Debug)]
 struct KubeContext {
     name: String,
     context: KubeContextMetadata,
 }
 
+#[derive(Clone, Debug)]
 struct KubeConfig {
     current_context: String,
     contexts: Vec<KubeContext>,
 }
 
+#[derive(Clone, Debug)]
 struct KubeMetadata {
     current_context: String,
     current_namespace: String,
 }
 
-fn extract_metadata(kube_config: KubeConfig, command: Vec<&String>) -> KubeMetadata {
+fn extract_metadata(kube_config: KubeConfig, command: Vec<String>) -> KubeMetadata {
     let mut context_from_command = None;
     let mut namespace_from_command = None;
 
     let mut command_iter = command.iter();
-    while let Some(&fragment) = command_iter.next() {
+    while let Some(fragment) = command_iter.next() {
         if fragment == "--context" {
             context_from_command = command_iter.next().map(|it| it.to_string());
         }
@@ -63,6 +65,35 @@ fn extract_metadata(kube_config: KubeConfig, command: Vec<&String>) -> KubeMetad
     KubeMetadata {
         current_context,
         current_namespace,
+    }
+}
+
+fn read_kube_config() -> KubeConfig {
+    let path = env::var("KUBECONFIG").unwrap_or(format!(
+        "{}/.kube/config",
+        env::var("HOME").unwrap_or("~".to_string())
+    ));
+    let contents = fs::read_to_string(path).expect("could not read kubeconfig");
+
+    let documents = YamlLoader::load_from_str(&contents).unwrap();
+    let document = &documents[0];
+
+    let contexts = &document["contexts"]
+        .clone()
+        .into_iter()
+        .map(|context| KubeContext {
+            name: context["name"].as_str().unwrap().to_string(),
+            context: KubeContextMetadata {
+                namespace: context["context"]["namespace"]
+                    .as_str()
+                    .map(|it| it.to_string()),
+            },
+        })
+        .collect::<Vec<_>>();
+
+    KubeConfig {
+        current_context: document["current_context"].as_str().unwrap().to_string(),
+        contexts: contexts.clone(),
     }
 }
 
